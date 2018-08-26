1037d31ed5b491f6ea1314736d6b0370e185673c
vladislav.kovchug <vladislav.kovchug@teamdev.com>
2018-08-26 14:51:04 +0300

    Add material components

--- front/food-order/src/app/app.module.ts
+++ front/food-order/src/app/app.module.ts
@@ -2,13 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrderTableComponent } from './order-table/order-table.component';
+import { RouterModule, Routes } from '@angular/router';
+import { OrderComponent } from './pages/order/order.component';
+import { HistoryComponent } from './pages/history/history.component';
+
+const appRoutes: Routes = [
+  { path: 'new-order', component: OrderComponent },
+  { path: 'history', component: HistoryComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    DashboardComponent,
+    OrderTableComponent,
+    OrderComponent,
+    HistoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
