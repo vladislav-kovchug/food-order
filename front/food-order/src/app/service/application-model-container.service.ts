fef29d967836e6b803fa53b0ef53e400d731e885
vladislav.kovchug <vladislav.kovchug@teamdev.com>
2018-09-10 16:26:58 +0300

    Remove irrelevant FoodItem class

--- front/food-order/src/app/service/application-model-container.service.ts
+++ front/food-order/src/app/service/application-model-container.service.ts
@@ -1,19 +1,9 @@
 import { Injectable } from '@angular/core';
-import {FoodItem} from "../model/food-item";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApplicationModelContainer {
-  private foodItems: Array<FoodItem>;
-
-  public getFoodItems() {
-    return this.foodItems;
-  }
-
-  public updateFoodItems(foodItems: Array<FoodItem>) {
-    this.foodItems = foodItems;
-  }
 
   constructor() { }
 }
