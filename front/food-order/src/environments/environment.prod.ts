3eefebe7a731bf48b0a3f8b598ccbf058610dc93
vladislav.kovchug <vladislav.kovchug@teamdev.com>
2018-09-10 16:23:53 +0300

    Add prod enviroment variables.

--- front/food-order/src/environments/environment.prod.ts
+++ front/food-order/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDR3YiqV-OlDFsPyhphD7M41fwnxcrrn_U",
+    authDomain: "food-order-8e485.firebaseapp.com",
+    databaseURL: "https://food-order-8e485.firebaseio.com",
+    projectId: "food-order-8e485",
+    storageBucket: "food-order-8e485.appspot.com",
+    messagingSenderId: "262760377594"
+  }
 };
