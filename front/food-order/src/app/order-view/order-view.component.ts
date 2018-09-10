8ec7fc0086a409bbcaa034f734d9b94678ab3746
vladislav.kovchug <vladislav.kovchug@teamdev.com>
2018-09-10 16:14:58 +0300

    Move group order view into separate component.

---
+++ front/food-order/src/app/order-view/order-view.component.ts
@@ -0,0 +1,67 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Order} from "../model/order";
+import {UserData} from "../model/user-data";
+import {OrderItem} from "../model/order-item";
+
+@Component({
+  selector: 'app-order-view',
+  templateUrl: './order-view.component.html',
+  styleUrls: ['./order-view.component.scss']
+})
+export class OrderViewComponent implements OnInit {
+  private _groupOrder: { [userId: string]: Order };
+  private _users: { [userId: string]: UserData };
+  orders: {
+    user: UserData,
+    items: OrderItem[],
+    totalCost: number,
+    comments: string
+  }[] = [];
+
+  constructor() {
+
+  }
+
+  @Input("order")
+  set order2(groupOrder: { [userId: string]: Order }) {
+    this._groupOrder = groupOrder;
+    this.updateOrders();
+  }
+
+  @Input("users")
+  set users2(users: { [userId: string]: UserData }) {
+    this._users = users;
+    this.updateOrders();
+  }
+
+  ngOnInit() {
+
+  }
+
+  private updateOrders() {
+    this.orders = [];
+    if (!this._users || !this._groupOrder) {
+      return;
+    }
+
+    Object.keys(this._groupOrder).forEach((key) => {
+      let user = this._users[key];
+      let comments = this._groupOrder[key].comments;
+      let items: OrderItem[] = [];
+      let totalCost = 0;
+      Object.keys(this._groupOrder[key].items).forEach((itemKey) => {
+        let orderItem = this._groupOrder[key].items[itemKey];
+        totalCost += orderItem.price;
+        items.push(orderItem);
+      });
+
+      this.orders.push({
+        user: user,
+        items: items,
+        totalCost: totalCost,
+        comments: comments
+      })
+    });
+  }
+
+}
--- front/food-order/src/app/pages/order/proceed-order/proceed-order.component.ts
+++ front/food-order/src/app/pages/order/proceed-order/proceed-order.component.ts
@@ -1,8 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from "@angular/material";
 import {ProceedOrderDialogData} from "./proceed-order-dialog-data";
-import {UserData} from "../../../model/user-data";
-import {OrderItem} from "../../../model/order-item";
 
 @Component({
   selector: 'app-proceed-order',
@@ -10,32 +8,7 @@ import {OrderItem} from "../../../model/order-item";
   styleUrls: ['./proceed-order.component.scss']
 })
 export class ProceedOrderComponent implements OnInit {
-  orders: {
-    user: UserData,
-    items: OrderItem[],
-    totalCost: number,
-    comments: string
-  }[] = [];
-
   constructor(@Inject(MAT_DIALOG_DATA) public dialogData: ProceedOrderDialogData) {
-    Object.keys(this.dialogData.activeOrder).forEach((key) => {
-      let user = this.dialogData.users[key];
-      let comments = this.dialogData.activeOrder[key].comments;
-      let items: OrderItem[] = [];
-      let totalCost = 0;
-      Object.keys(this.dialogData.activeOrder[key].items).forEach((itemKey) => {
-        let orderItem = this.dialogData.activeOrder[key].items[itemKey];
-        totalCost += orderItem.price;
-        items.push(orderItem);
-      });
-
-      this.orders.push({
-        user: user,
-        items: items,
-        totalCost: totalCost,
-        comments: comments
-      })
-    });
 
   }
 
