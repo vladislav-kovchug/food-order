4612574daa1c728f5ccc16da102f1e82eba2ae21
galanovnick <galanov.nick@gmail.com>
2020-01-13 11:40:30 +0200

    Replace `onRequest` function format to `scheduled`

--- functions/src/index.js
+++ functions/src/index.js
@@ -10,40 +10,38 @@ const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 const MENU_PATH = "/menu";
 const MENU_UPDATE_TIMESTAMP_PATH = "/menuUpdateTimestamp";
 
-exports.loadMenu = functions.https.onRequest(async (req, res) => {
-    const auth = await google.auth.getClient({scopes: SCOPES});
-    const sheetsApi = google.sheets({version: "v4", auth});
+// Every Monday at 11:00AM
+exports.loadMenu = functions.pubsub.schedule("00 11 * * mon")
+    .timeZone("Europe/Kiev")
+    .onRun(async context => {
+        const auth = await google.auth.getClient({scopes: SCOPES});
+        const sheetsApi = google.sheets({version: "v4", auth});
 
-    const sheetIdResult = await getSheetId();
-    if (!sheetIdResult || !sheetIdResult.val) {
-        const message = `Failed to retrieve sheet id from Firebase`;
-        console.error(message);
-        res.send(message);
-        return res.status(200).end();
-    }
+        const sheetIdResult = await getSheetId();
+        if (!sheetIdResult || !sheetIdResult.val) {
+            const message = `Failed to retrieve sheet id from Firebase`;
+            console.error(message);
+            return;
+        }
 
-    const sheetId = sheetIdResult.val();
+        const sheetId = sheetIdResult.val();
 
-    const sheetResult = await read(sheetsApi, sheetId);
-    if (!sheetResult || !sheetResult.data || !sheetResult.data.values) {
-        const message = `Failed to read data from sheet ${sheetId}`;
-        console.error(message);
-        res.send(message);
-        return res.status(200).end();
-    }
+        const sheetResult = await read(sheetsApi, sheetId);
+        if (!sheetResult || !sheetResult.data || !sheetResult.data.values) {
+            const message = `Failed to read data from sheet ${sheetId}`;
+            console.error(message);
+            return;
+        }
 
-    console.info(`Creating menu...`);
-    const menu = createMenu(sheetResult.data.values);
+        console.info(`Creating menu...`);
+        const menu = createMenu(sheetResult.data.values);
 
-    console.info(`Writing menu to Firebase ${MENU_PATH}...`);
-    await admin.database().ref(MENU_PATH).set(menu);
-    await admin.database().ref(MENU_UPDATE_TIMESTAMP_PATH).set(Date.now());
+        console.info(`Writing menu to Firebase ${MENU_PATH}...`);
+        await admin.database().ref(MENU_PATH).set(menu);
+        await admin.database().ref(MENU_UPDATE_TIMESTAMP_PATH).set(Date.now());
 
-    console.info("done");
-
-    res.send("done");
-    return res.status(200).end();
-});
+        console.info("done");
+    });
 
 function read(sheetsApi, spreadsheetId) {
     console.info(`Fetching data from sheet ${spreadsheetId}...`);
